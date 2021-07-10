4977fa940a25f1c7e524706a0aa2245d7e8f67a2
Sacatucarrera <69715500+Sacatucarrera@users.noreply.github.com>
2021-07-10 20:19:22 +0200

    Add files via upload

--- Coche.java
+++ Coche.java
@@ -35,6 +35,20 @@ boolean automatico = true;
 		ferrari.setMatricula("Aprobadisimos");
 		String matriculaNueva = ferrari.getMatricula();
 		System.out.println(matriculaNueva);
+		
+		//Demostración de las sentencias de código if-else-else if
+		Coche trailer = new Coche(8,"AAA");
+		Coche furgoneta = new Coche(3,"BBB");
+		Coche tesla = new Coche(4,"CCC");
+		
+		comparaRuedasCoches(ferrari,trailer);
+		comparaRuedasCoches(ferrari,furgoneta);
+		comparaRuedasCoches(ferrari,tesla);
+		
+		reduceRuedasCoche(furgoneta,trailer);
+		
+		reduceRuedasCoche(furgoneta,trailer);
+		
 	}
 	
 	//Getter
@@ -59,6 +73,68 @@ boolean automatico = true;
 	 */
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
+	}
+	
+	//Metodo comparativo de ruedas de coches
+	public static void comparaRuedasCoches(Coche a, Coche b) {
+		
+		//Lo primero es coger las ruedas de los coches
+		int NumeroRuedasA = a.getNumeroRuedas();
+		int NumeroRuedasB = b.getNumeroRuedas();
+		
+		//Secuencia de control if-else-else if
+		if(NumeroRuedasA>NumeroRuedasB) {
+			System.out.println("El numero de ruedas del primer coche es mayor que el numero de ruedas del segundo coche.");
+		}
+		else if(NumeroRuedasA == NumeroRuedasB) {
+			System.out.println("El numero de ruedas de ambos coches es el mismo.");
+		}
+		else {
+			System.out.println("El numero de ruedas del segundo coche es mayor que el numero de ruedas del primer coche.");
+		}
+		
+	}
+	
+	public static void reduceRuedasCoche(Coche a, Coche b) {
+		
+		//Secuencia de control while
+		while(a.getNumeroRuedas() != b.getNumeroRuedas()){
+			
+			//Si el numero de ruedas del coche a es mayor que el del coche b
+			if(a.getNumeroRuedas()>b.getNumeroRuedas()) {
+				
+				//Comparativa de la paridad o imparidad del numero de ruedas
+				if((a.getNumeroRuedas()%2) == (b.getNumeroRuedas()%2)) {
+					
+					//Si son ambos numeros de ruedas pares o impares resto de dos en dos
+					a.setNumeroRuedas(a.getNumeroRuedas()-2);
+					
+				}
+				
+				//Si no lo hago de uno en uno
+				else {
+					a.setNumeroRuedas(a.getNumeroRuedas()-1);
+				}
+				
+			}
+			
+			//Caso contrario, el numero de ruedas del coche b sera mayor al de a
+			else {
+				
+				if((a.getNumeroRuedas()%2) == (b.getNumeroRuedas()%2)) {
+					
+					b.setNumeroRuedas(b.getNumeroRuedas()-2);
+					
+				}
+				
+				else {
+					b.setNumeroRuedas(b.getNumeroRuedas()-1);
+				}
+				
+			}
+			
+		} 
+		
 	}
 	
 }
